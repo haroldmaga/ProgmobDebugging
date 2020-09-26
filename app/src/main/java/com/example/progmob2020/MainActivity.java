82be005beec16143d6f1e11c423ed2dc8fa838b2
harold <haroldmaga16@gmail.com>
2020-09-27 00:52:29 +0800

    HelloWorld

---
+++ app/src/main/java/com/example/progmob2020/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.progmob2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*TextView textview = (TextView) findViewById(R.id.mainActivityTextView);
+        textview.setText("Test Untuk Update VIew");*/
+
+
+    }
+}
