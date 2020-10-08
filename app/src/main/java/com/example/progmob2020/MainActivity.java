719a9555c69d4b8e135c2359dc1b824358d27bff
haroldimmanuel <haroldmaga16@gmail.com>
2020-10-09 00:22:28 +0800

    tugas 2

--- app/src/main/java/com/example/progmob2020/MainActivity.java
+++ app/src/main/java/com/example/progmob2020/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.progmob2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +17,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*TextView textview = (TextView) findViewById(R.id.mainActivityTextView);
-        textview.setText("Test Untuk Update VIew");*/
+
+        //Variabell
+        final TextView textview = (TextView) findViewById(R.id.mainActivityTextView);
+        Button myBtn = (Button)findViewById((R.id.button1));
+        final EditText myEditText = (EditText)findViewById(R.id.editText1);
+        Button btnHelp  = (Button)findViewById(R.id.btnHelp);
+
+        //Action
+        textview.setText(R.string.test_hello_world);
+        myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.d("Coba CLICKKK", myEditText.getText().toString());
+                textview.setText(myEditText.getText().toString());
+            }
+        });
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,HelpActivity.class);
+                Bundle b =  new Bundle();
+
+                b.putString("helpString",myEditText.getText().toString());
+                intent.putExtras(b);
+
+                startActivity(intent);
+            }
+        });
 
 
     }
---
+++ app/src/main/java/com/example/progmob2020/ProteinTrackerApp.java
@@ -0,0 +1,14 @@
+package com.example.progmob2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProteinTrackerApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_protein_tracker_app);
+    }
+}
