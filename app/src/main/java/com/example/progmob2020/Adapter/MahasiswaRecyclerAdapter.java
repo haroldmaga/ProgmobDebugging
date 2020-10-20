1147c77a602f7337f70064c7ca27724a0fbd4eda
haroldimmanuel <haroldmaga16@gmail.com>
2020-10-20 17:47:04 +0800

    tugas 3

---
+++ app/src/main/java/com/example/progmob2020/Adapter/MahasiswaRecyclerAdapter.java
@@ -0,0 +1,70 @@
+package com.example.progmob2020.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.progmob2020.Model.Mahasiswa;
+import com.example.progmob2020.Model.Mahasiswa;
+import com.example.progmob2020.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MahasiswaRecyclerAdapter extends RecyclerView.Adapter<MahasiswaRecyclerAdapter.ViewHolder> {
+    private Context context;
+    private List<Mahasiswa> mahasiswaList;
+
+    public MahasiswaRecyclerAdapter(Context context) {
+        this.context = context;
+        mahasiswaList = new ArrayList<>();
+    }
+
+
+    public List<Mahasiswa> getMahasiswaList() {
+        return mahasiswaList;
+    }
+
+    public void setMahasiswaList(List<Mahasiswa> mahasiswaList) {
+        this.mahasiswaList = mahasiswaList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.item_list_cardview,parent,false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Mahasiswa m = mahasiswaList.get((position));
+
+        holder.tvNama.setText(m.getNama());
+        holder.tvNim.setText(m.getNim());
+        holder.tvNoTlp.setText(m.getNotelp());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mahasiswaList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView tvNama, tvNim, tvNoTlp;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvNama = itemView.findViewById(R.id.tvNama);
+            tvNim = itemView.findViewById(R.id.tvNim);
+            tvNoTlp = itemView.findViewById(R.id.tvNoTlp);
+        }
+    }
+}
